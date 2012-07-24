25ef1c58fd5750c8a5eb61db38203b720d147021
hello@andrewrose.co.uk <root@pluto.localdomain>
2012-07-24 14:29:02 +0100

    Various updates: license, now daemonizes, README updates for requirements, start of stats added.

--- cached.php
+++ cached.php
@@ -1,5 +1,24 @@
 #!/usr/bin/php
 <?php
+/*
+ This file is part of cached.php 
+ http://github.com/AndrewRose/cached.php
+ License: GPL; see below
+ Copyright Andrew Rose (hello@andrewrose.co.uk) 2012
+
+    cached.php is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    cached.php is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with cached.php.  If not, see <http://www.gnu.org/licenses/>
+*/
 
 namespace Cached;
 
@@ -8,11 +27,60 @@ class Handler
 	public $connections = [];
 	public $buffers = [];
 	public $maxRead = 256;
-	private $backend = FALSE;
 	private $data = [];
+	private $stats = [
+		'pid'                   => 0,     // 32u - Process id of this server process
+		'uptime'                => 0,     // 32u - Number of secs since the server started
+		'time'                  => 0,     // 32u  - current UNIX time according to the server
+		'version'               => '',    // string - Version string of this server
+		'pointer_size'          => 0,     // 32 - Default size of pointers on the host OS (generally 32 or 64)
+		'rusage_user'           => 0,     // 32u.32u - Accumulated user time for this process (seconds:microseconds)
+		'rusage_system'         => 0,     // 32u.32u - Accumulated system time for this process (seconds:microseconds)
+		'curr_items'            => 0,     // 32u - Current number of items stored
+		'total_items'           => 0,     // 32u - Total number of items stored since the server started
+		'bytes'                 => 0,     // 64u - Current number of bytes used to store items
+		'curr_connections'      => 0,     // 32u - Number of open connections
+		'total_connections'     => 0,     // 32u  - Total number of connections opened since the server started running
+		'connection_structures' => 0,     // 32u - Number of connection structures allocated by the server
+		'reserved_fds'          => 0,     // 32u - Number of misc fds used internally
+		'cmd_get'               => 0,     // 64u - Cumulative number of retrieval reqs
+		'cmd_set'               => 0,     // 64u - Cumulative number of storage reqs
+		'cmd_flush'             => 0,     // 64u - Cumulative number of flush reqs
+		'cmd_touch'             => 0,     // 64u - Cumulative number of touch reqs
+		'get_hits'              => 0,     // 64u - Number of keys that have been requested and found present
+		'get_misses'            => 0,     // 64u - Number of items that have been requested and not found
+		'delete_misses'         => 0,     // 64u - Number of deletions reqs for missing keys
+		'delete_hits'           => 0,     // 64u - Number of deletion reqs resulting in an item being removed.
+		'incr_misses'           => 0,     // 64u - Number of incr reqs against missing keys.
+		'incr_hits'             => 0,     // 64u - Number of successful incr reqs.
+		'decr_misses'           => 0,     // 64u - Number of decr reqs against missing keys.
+		'decr_hits'             => 0,     // 64u - Number of successful decr reqs.
+		'cas_misses'            => 0,     // 64u - Number of CAS reqs against missing keys.
+		'cas_hits'              => 0,     // 64u - Number of successful CAS reqs.
+		'cas_badval'            => 0,     // 64u - Number of CAS reqs for which a key was found, but the CAS value did not match.
+		'touch_hits'            => 0,     // 64u - Numer of keys that have been touched with a new expiration time
+		'touch_misses'          => 0,     // 64u - Numer of items that have been touched and not found
+		'auth_cmds'             => 0,     // 64u - Number of authentication commands handled, success or failure.
+		'auth_errors'           => 0,     // 64u - Number of failed authentications.
+		'evictions'             => 0,     // 64u - Number of valid items removed from cache to free memory for new items
+		'reclaimed'             => 0,     // 64u - Number of times an entry was stored using memory from an expired entry
+		'bytes_read'            => 0,     // 64u - Total number of bytes read by this server from network
+		'bytes_written'         => 0,     // 64u - Total number of bytes sent by this server to network
+		'limit_maxbytes'        => 0,     // 32u - Number of bytes this server is allowed to use for storage.
+		'threads'               => 0,     // 32u - Number of worker threads requested. (see doc/threads.txt)
+		'conn_yields'           => 0,     // 64u - Number of times any connection yielded to another due to hitting the -R limit.
+		'hash_power_level'      => 0,     // 32u - Current size multiplier for hash table
+		'hash_bytes'            => 0,     // 64u - Bytes currently used by hash tables
+		'hash_is_expanding'     => FALSE, // bool - Indicates if the hash table is being grown to a new size
+		'expired_unfetched'     => 0,     // 64u - Items pulled from LRU that were never touched by get/incr/append/etc before expiring
+		'evicted_unfetched'     => 0,     // 64u - Items evicted from LRU that were never touched by get/incr/append/etc.
+		'slab_reassign_running' => FALSE, // bool - If a slab page is being moved
+		'slabs_moved'           => 0      // 64u - Total slab pages moved
+	];
 
-	public function __construct()
+	public function __construct($pid)
 	{
+		$this->stats['pid'] = $pid;
 		$this->db = new \PDO('mysql:host=localhost;dbname=cached', 'root', '');
 		$this->dbStmtInsert = $this->db->prepare("INSERT INTO cache(k, data, flags) VALUES(:k, :data, :flags)");
 
@@ -141,9 +209,23 @@ class Handler
 				}
 				else
 				{
-					//list($key, $flags, $exptime, $bytes, $noreply) = explode(' ', $line);
-					list($key, $flags, $exptime, $bytes) = explode(' ', $line);
-					
+					$args = explode(' ', $line);
+					$argsCount = sizeof($args);
+
+					if($argsCount == 4)
+					{
+						list($key, $flags, $exptime, $bytes) = $args;
+					}
+					else if($argsCount == 5)
+					{
+						list($key, $flags, $exptime, $bytes, $noreply) = $args;
+					}
+					else
+					{
+						$this->ev_write($id, "CLIENT_ERROR unexpected number of command arguments, expected 4 or 5 but got: ".$argsCount."\r\n");
+						return;
+					}
+
 					if($cmd == 'set' ||
 						($cmd == 'add' && !isset($this->data[$key])) ||
 						($cmd == 'replace' && isset($this->data[$key])))
@@ -185,6 +267,12 @@ class Handler
 			}
 			break;
 
+			case 'stats':
+			{
+
+			}
+			break;
+
 			default:
 			{
 				echo 'unknown command: '.$line."\n";
@@ -194,4 +282,9 @@ class Handler
 	}
 }
 
-new Handler();
+$pid = pcntl_fork();
+if($pid)
+{
+    exit();
+}
+new Handler($pid);
